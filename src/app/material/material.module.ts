5db63c59c69237d74ac8725785713a4e6b05abd5
Abhinav Nandekar <abhinavnandekar@gmail.com>
2024-05-10 12:53:51 +0530

    abhinav mat

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,79 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatBadgeModule } from '@angular/material/badge';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatListModule } from '@angular/material/list';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSelectModule } from '@angular/material/select';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormControl, FormGroupDirective, NgForm, Validators, ReactiveFormsModule,} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import {MatButton} from '@angular/material/button';
+import {MatTooltip} from '@angular/material/tooltip';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatDividerModule,
+    MatBadgeModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatButtonToggleModule,
+    MatChipsModule,
+    MatListModule,
+    MatExpansionModule,
+    MatSelectModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    FormControl,
+    FormGroupDirective,
+    NgForm,
+    Validators,
+    ReactiveFormsModule,
+    ErrorStateMatcher,
+    MatButton,
+    MatTooltip,
+
+
+
+
   ]
 })
 export class MaterialModule { }
