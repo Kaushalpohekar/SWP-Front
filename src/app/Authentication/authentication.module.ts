bdcdb06bfa49061ffb5d0a3def44c62c6750097c
Kaushalpohekar <Kaushalpohekar1@gmail.com>
2024-05-10 12:32:11 +0530

    Kaushal

---
+++ src/app/Authentication/authentication.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthenticationLayoutComponent } from './authentication-layout/authentication-layout.component';
+
+
+
+@NgModule({
+  declarations: [
+    AuthenticationLayoutComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuthenticationModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationLayoutComponent } from './Authentication/authentication-layout/authentication-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    component: AuthenticationLayoutComponent,
+    children: [
+      { path: '', loadChildren: () => import('./Authentication/authentication.module').then(m => m.AuthenticationModule) },
+    ]
+  },
+  // {
+  //   path: 'dash',
+  //   component: DashLayoutComponent,
+  //   children: [
+  //     { path: '', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+  //   ]
+  // },
+  // {
+  //   path: 'sa',
+  //   component: SALayoutComponent,
+  //   children: [
+  //     { path: '', loadChildren: () => import('./super-admin/super-admin.module').then(m => m.SuperAdminModule) },
+  //   ]
+  // },
+  { path: '**', redirectTo: 'login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
