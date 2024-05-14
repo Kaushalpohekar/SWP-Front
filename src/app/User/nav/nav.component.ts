079d54382cd5a57b9a10a28d49fe376b7c19d2f7
Kaushalpohekar <Kaushalpohekar1@gmail.com>
2024-05-14 18:35:52 +0530

    Kaushal

--- src/app/User/nav/nav.component.ts
+++ src/app/User/nav/nav.component.ts
@@ -1,4 +1,8 @@
-import { Component} from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
@@ -6,4 +10,11 @@ import { Component} from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 }
