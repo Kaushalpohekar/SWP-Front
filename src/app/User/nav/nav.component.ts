ed9f28e91ff83ac03544988234f097b12edbbb47
Kaushalpohekar <Kaushalpohekar1@gmail.com>
2024-05-14 15:49:46 +0530

    Kaushal

--- src/app/User/nav/nav.component.ts
+++ src/app/User/nav/nav.component.ts
@@ -1,8 +1,4 @@
-import { Component, inject } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
-import { AsyncPipe, NgIf } from '@angular/common';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -10,11 +6,4 @@ import { AsyncPipe, NgIf } from '@angular/common';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
-  private breakpointObserver = inject(BreakpointObserver);
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
 }
--- src/app/User/user-layout-component/user-layout-component.component.ts
+++ src/app/User/user-layout-component/user-layout-component.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-user-layout-component',
@@ -6,5 +10,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-layout-component.component.css']
 })
 export class UserLayoutComponentComponent {
+  private breakpointObserver = inject(BreakpointObserver);
 
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 }
