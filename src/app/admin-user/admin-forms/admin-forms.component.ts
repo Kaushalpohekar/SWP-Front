660ef99d80a2e96ebd0b5abbd694073b23b0c7e2
Krishna Lokhande <133361024+Krisssss987@users.noreply.github.com>
2024-06-10 10:40:46 +0530

    forms

--- src/app/admin-user/admin-forms/admin-forms.component.ts
+++ src/app/admin-user/admin-forms/admin-forms.component.ts
@@ -15,16 +15,61 @@ export class AdminFormsComponent {
   isLinear = false;
   firstFormGroup!: FormGroup;
   secondFormGroup!: FormGroup;
+  numberOfQuestions!:number;
 
   constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
-    });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
+    this.forms();
+    this.firstFormGroup.get("Questions")?.valueChanges.subscribe(value => {
+      this.numberOfQuestions = value;
+      this.addQuestions();
     });
   }
-  
+
+  forms() {
+    this.firstFormGroup = this._formBuilder.group({
+      FormName: ['', Validators.required],
+      FormDescription: ['', Validators.required],
+      Questions: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      QuestionOne: ['', Validators.required],
+      QuestionsOneType: ['', Validators.required],
+      OptionOne: ['', Validators.required]
+    });
+  }
+
+  addQuestions() {
+    // Clear existing controls
+    Object.keys(this.secondFormGroup.controls).forEach(key => {
+      this.secondFormGroup.removeControl(key);
+    });
+
+    // Add controls based on the updated number of questions
+    for (let i = 1; i <= this.numberOfQuestions; i++) {
+      this.secondFormGroup.addControl(`Question${i}`, this._formBuilder.control('', Validators.required));
+      this.secondFormGroup.addControl(`Question${i}Type`, this._formBuilder.control('', Validators.required));
+      this.secondFormGroup.addControl(`Option${i}`, this._formBuilder.control('', Validators.required));
+    }
+  }
+
+  getQuestionType(i: number): string {
+    return this.secondFormGroup.get(`Question${i}Type`)?.value;
+  }
+
+  getQuestionsAndAnswers() {
+    const questionsArray = [];
+    for (let i = 1; i <= this.numberOfQuestions; i++) {
+      const question = this.secondFormGroup.get(`Question${i}`)?.value;
+      const questionType = this.secondFormGroup.get(`Question${i}Type`)?.value;
+      const options = this.secondFormGroup.get(`Option${i}`)?.value;
+      questionsArray.push({
+        question: question,
+        questionType: questionType,
+        options: options
+      });
+    }
+    console.log(questionsArray);
+  }  
 }
